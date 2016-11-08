159fc7f1768bcd0ebd5a45e876ec29aa18e51ed7
Sanjida Gafur <sanjida.gafur@gmail.com>
2016-11-08 14:39:57 +0000

    Initial commit from the atomist bot

---
+++ src/main/java/com/roguenation/RogueServiceApplication.java
@@ -0,0 +1,12 @@
+package com.roguenation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RogueServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/com/roguenation/RogueServiceConfiguration.java
@@ -0,0 +1,9 @@
+package com.roguenation;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RogueServiceConfiguration {
+    // Your beans go here ...
+    // Atomist Bot suggests "maybe use an Atomist project editor to add a sample one?"
+}
